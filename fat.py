29d023e35ef8e2c1f55d987e84b486d6e120c197
prithvi-patel-tamu <prith.vi@tamu.edu>
2023-11-15 12:02:53 -0600

    FAT added

---
+++ fat.py
@@ -0,0 +1,60 @@
+import argparse
+
+def main():
+    # Create the top-level parser
+    parser = argparse.ArgumentParser(prog='fat', description='Flow Analysis Tool (F.A.T.)')
+    
+    # Define global options
+    parser.add_argument('--version', action='version', version='%(prog)s 2.0')
+
+    # Create subparsers for the different commands
+    subparsers = parser.add_subparsers(dest='command', help='commands')
+
+    # Analyze command
+    analyze_parser = subparsers.add_parser('analyze', aliases=['a'], help='Initiates the flow analysis process')
+    analyze_parser.add_argument('path', help='Path to the source code')
+    analyze_parser.add_argument('--function', '-f', help='Target a specific function for detailed flow analysis')
+    analyze_parser.add_argument('--verbose', '-v', action='store_true', help='Provide detailed logs')
+    analyze_parser.add_argument('--output', '-o', help='Specify file path for the analysis output')
+
+    # Generate UML command
+    generate_parser = subparsers.add_parser('generate-uml', aliases=['g'], help='Generates UML diagrams')
+    generate_parser.add_argument('path', help='Path to the source code')
+    generate_parser.add_argument('--class', '-c', help='Target a specific class for dependency graph generation')
+    generate_parser.add_argument('--output', '-o', help='Specify file path for the diagram output')
+    generate_parser.add_argument('--quiet', '-q', action='store_true', help='Run in quiet mode')
+
+    # Dependencies command
+    dependencies_parser = subparsers.add_parser('dependencies', aliases=['d'], help='Analyze and show dependencies')
+    dependencies_parser.add_argument('path', help='Path to the source code root directory')
+
+    # Help command is automatically added by argparse
+
+    # Parse the arguments
+    args = parser.parse_args()
+    
+    # Handle the commands
+    if args.command == 'analyze' or args.command == 'a':
+        handle_analyze(args)
+    elif args.command == 'generate-uml' or args.command == 'g':
+        handle_generate_uml(args)
+    elif args.command == 'dependencies' or args.command == 'd':
+        handle_dependencies(args)
+    else:
+        parser.print_help()
+
+def handle_analyze(args):
+    # Implement the analysis logic here
+    print(f"Analyzing {args.path}")
+    
+
+def handle_generate_uml(args):
+    # Implement the UML generation logic here
+    print(f"Generating UML for {args.path}")
+
+
+def handle_dependencies(args):
+    # Implement the dependencies handling logic here
+    print(f"Analyzing dependencies for {args.path}")
+    
+	
