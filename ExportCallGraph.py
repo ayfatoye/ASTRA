4ea03c883dad2d87cb26d5c540520557cab0861a
Sebastian Oberg <Sebastianoberg2002@tamu.edu>
2023-11-29 14:44:13 -0600

    Worked on getting call graph export code working (WIP)

--- ExportCallGraph.py
+++ ExportCallGraph.py
@@ -1,31 +1,46 @@
+import argparse
 import subprocess
-import json
 import os
 
-# Path to the Ghidra project
-ghidra_project_path = "/path/to/ghidra/project"
-# Path to the binary file you want to analyze
-binary_path = "/path/to/binary/file"
-# Path to the Ghidra installation
-ghidra_path = "/path/to/ghidra"
-# Path to the Ghidra script that extracts the call flow graph
-ghidra_script_path = "/path/to/ghidra/script"
+def run_ghidra_analysis(binary_path, output_path, ghidra_path, ghidra_script_path):
+    # Resolve output path to an absolute path
+    output_path = os.path.abspath(output_path)
 
-# Running Ghidra in headless mode
-headless_command = [
-    f"{ghidra_path}/support/analyzeHeadless",
-    ghidra_project_path,
-    "project_name",  # You can specify a project name
-    "-import", binary_path,
-    "-postScript", ghidra_script_path,
-    "-deleteProject"
-]
+    # Check if the output directory is empty
+    output_dir = os.path.dirname(output_path)
+    if not output_dir:
+        output_dir = '.'
 
-subprocess.run(headless_command)
+    # Ensure output directory exists
+    os.makedirs(output_dir, exist_ok=True)
 
-# Assuming your Ghidra script outputs the call graph in JSON format to a file
-output_json_path = "/path/to/output.json"
+    # Running Ghidra in headless mode
+    headless_command = [
+        os.path.join(ghidra_path, "support", "analyzeHeadless"),
+        ghidra_path,  # Ghidra project directory
+        "temp_project",  # Temporary project name
+        "-import", binary_path,
+        "-postScript", ghidra_script_path,
+        "-scriptPath", os.path.dirname(ghidra_script_path),
+        "-deleteProject",
+        "-readOnly"
+    ]
 
-# Read the JSON output
-with open(output_json_path, 'r') as file:
-    call_graph = json.load(file)
+    subprocess.run(headless_command)
+    print("Call graph exported to {}".format(output_path))
+
+def main():
+    parser = argparse.ArgumentParser(description='Binary Analysis Tool')
+    parser.add_argument('--analyze', help='Path to binary for analysis')
+    parser.add_argument('--output', help='Output file path for the analysis report')
+    parser.add_argument('--ghidra', help='Path to Ghidra installation')
+    args = parser.parse_args()
+
+    if args.analyze and args.output and args.ghidra:
+        ghidra_script_path = os.path.join(os.path.dirname(__file__), "ExportCallGraph.py")
+        run_ghidra_analysis(args.analyze, args.output, args.ghidra, ghidra_script_path)
+    else:
+        print("Missing arguments. Use --help for more information.")
+
+if __name__ == "__main__":
+    main()
